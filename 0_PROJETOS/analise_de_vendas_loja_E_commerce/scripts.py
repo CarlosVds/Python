45a6ba7284fd56e00e5dac7eee59cbdc6d19c453
CarlosVds <carloseduardovds@gmail.com>
2025-11-13 17:54:00 -0300

    Iniciando mini projeto Analise de vendas loja E-commerce

--- 0_PROJETOS/analise_de_vendas_loja_E_commerce/scripts.py
+++ 0_PROJETOS/analise_de_vendas_loja_E_commerce/scripts.py
@@ -1,8 +1,34 @@
-import pandas as pd
-import numpy as np
+import pandas as pd  
+import numpy as np  
 import matplotlib.pyplot as plt
-import seaborn as sns 
-import random
-from datetime import datetime, timedelta
+import seaborn as sns   
+import random  
+from datetime import datetime, timedelta  
 
 
+def gera_dados_fecticios(num_registros = 600):
+    
+    print(f"\nIniciando a geração de {num_registros} registros de vendas...")
+
+    produtos = {
+        'Laptop Gamer': {'categoria': 'Eletrônicos', 'preco': 7500.00},
+        'Mouse Vertical': {'categoria': 'Acessórios', 'preco': 250.00},
+        'Teclado Mecânico': {'categoria': 'Acessórios', 'preco': 550.00},
+        'Monitor Ultrawide': {'categoria': 'Eletrônicos', 'preco': 2800.00},
+        'Cadeira Gamer': {'categoria': 'Móveis', 'preco': 1200.00},
+        'Headset 7.1': {'categoria': 'Acessórios', 'preco': 800.00},
+        'Placa de Vídeo': {'categoria': 'Hardware', 'preco': 4500.00},
+        'SSD 1TB': {'categoria': 'Hardware', 'preco': 600.00}
+    }
+    
+    lista_produtos = list(produtos.keys())
+    
+    cidades_estados = {
+        'São Paulo': 'SP', 'Rio de Janeiro': 'RJ', 'Belo Horizonte': 'MG',
+        'Porto Alegre': 'RS', 'Salvador': 'BA', 'Curitiba': 'PR', 'Fortaleza': 'CE'
+    }
+    
+    list_cidades = list(cidades_estados.keys())
+
+   
+    
