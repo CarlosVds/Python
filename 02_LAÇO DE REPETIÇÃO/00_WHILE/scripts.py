b819677cf67589f598538350f7083727628f393a
CarlosVds <carloseduardovds@gmail.com>
2026-01-14 15:08:45 -0300

    Laços de repetição com WHILE

---
+++ 02_LAÇO DE REPETIÇÃO/00_WHILE/scripts.py
@@ -0,0 +1,32 @@
+# tabuada = input('Digite qual tabuada deseja saber: ')
+
+# tabuadaInt = int(tabuada)
+# contador = 0
+
+# while contador <= 10:
+    
+#     print(contador)
+    
+#     contador += 1
+    
+#     if contador == 6:
+#         print('Número 6 encontrado.')
+#         continue
+    
+    
+    
+    
+# while contador <= 10:
+#     print(f'{tabuadaInt} X {contador} = {contador * tabuadaInt}')
+    
+#     contador += 1
+
+nome = 'Carlos Santos'
+contador = 0
+
+while contador < len(nome):
+    
+    print(nome[contador])
+    
+    contador += 1
+    
