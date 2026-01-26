e7d6faf56f524666bc9ecb9543808f7dece96c95
CarlosVds <carloseduardovds@gmail.com>
2026-01-26 14:30:51 -0300

    Percorrendo lista com o FOR

--- 02_LAÇO DE REPETIÇÃO/01_FOR/scripts.py
+++ 02_LAÇO DE REPETIÇÃO/01_FOR/scripts.py
@@ -1,5 +1,10 @@
-numeros = range(0, 11)
-tabuada = 6
+# numeros = range(0, 11)
+# tabuada = 6
 
-for i in numeros:
-    print(f'{tabuada} X {i} = {i * tabuada}')
+# for i in numeros:
+#     print(f'{tabuada} X {i} = {i * tabuada}')
+
+lista = ['Carlos', "Enzo", 'Eika']
+
+for i in lista:
+    print(i, (type(lista)))
