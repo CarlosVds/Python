77989f793a77b2fa361ed96ebb237e82a49d7f9c
CarlosVds <carloseduardovds@gmail.com>
2026-01-26 14:25:13 -0300

    Trabalhando com lista

---
+++ 03_LISTAS/scripts.py
@@ -0,0 +1,14 @@
+lista_a = ['Carlos', 'Enzo']
+lista_b = lista_a.copy()
+# lista.append('Erika')
+# lista.insert(3, True)
+# lista.pop(3)
+# del lista[3]
+# lista.clear()
+# lista2 = ['Marcella']
+# lista.extend(lista2)
+
+lista_a[1] = 'Lista A, alterada'
+lista_b.append('Lista A copiada')
+print(lista_a)
+print(lista_b)
