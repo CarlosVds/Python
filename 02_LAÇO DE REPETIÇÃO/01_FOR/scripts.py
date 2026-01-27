a73246690c9bfa26a8897e471eb631823f3792b3
CarlosVds <carloseduardovds@gmail.com>
2026-01-27 14:03:14 -0300

    Interando com Tuple

--- 02_LAÇO DE REPETIÇÃO/01_FOR/scripts.py
+++ 02_LAÇO DE REPETIÇÃO/01_FOR/scripts.py
@@ -4,10 +4,27 @@
 # for i in numeros:
 #     print(f'{tabuada} X {i} = {i * tabuada}')
 
-lista = ['Carlos', "Enzo", 'Eika']
+lista = ['Carlos',"Enzo", 'Erika']
 
-lista.append('Cleide')
-lista.append('Alcino')
+# lista.append('Cleide')
+# lista.append('Alcino')
 
-for number, i in enumerate(lista):
-    print(number, i)
+# for number, i in enumerate(lista):
+#     print(number, i)
+
+listaTuple = tuple(lista)
+
+print(listaTuple.index('Erika'))
+
+# i = 0
+
+# while i < len(listaTuple):
+    
+#     nomeTuple = listaTuple[i]
+    
+#     contTuple = listaTuple.index('Enzo')
+    
+#     print(contTuple)
+    
+#     i += 1
+
