17ee1ca390259037cc09930a782ef6d40076f91b
CarlosVds <carloseduardovds@gmail.com>
2026-01-27 22:47:27 -0300

    For tabuada

--- 02_LAÇO DE REPETIÇÃO/01_FOR/scripts.py
+++ 02_LAÇO DE REPETIÇÃO/01_FOR/scripts.py
@@ -1,4 +1,10 @@
-texto = 'Pyhton'
+# texto = 'Pyhton'
 
-for i in texto:
-    print(i)
+# for i in texto:
+#     print(i)
+
+# numero = 56
+
+# print(f"Tabuada do {numero}:")
+# for i in range(1, 11):
+#     print(f"{numero} x {i} = {numero * i}")
