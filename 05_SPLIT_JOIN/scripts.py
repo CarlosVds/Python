6d527430fa5dfb4f6ea971da82d6d0a58e0880ee
CarlosVds <carloseduardovds@gmail.com>
2026-01-29 14:42:12 -0300

    Working whit Split, Join and Strip

---
+++ 05_SPLIT_JOIN/scripts.py
@@ -0,0 +1,18 @@
+# pharse = "         Python is very easy to learn, but without practice you won't learn it."
+
+# list_pharse = pharse.split(", ")
+
+# for i, pharse in enumerate(list_pharse):
+#     print(list_pharse[i].strip())
+
+# list_pharse_2 = ', '.join(list_pharse)
+
+# print(list_pharse_2.strip())
+
+list = 'Python'
+
+list_1 = 'p'
+
+list = list_1
+print(list)
+print(list_1)
