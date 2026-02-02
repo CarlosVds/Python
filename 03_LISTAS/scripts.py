df797da23959f5f2f70bc836b27d766e0e79576b
CarlosVds <carloseduardovds@gmail.com>
2026-02-02 18:02:10 -0300

    Lista recebendo um lista

--- 03_LISTAS/scripts.py
+++ 03_LISTAS/scripts.py
@@ -1,14 +1,31 @@
-lista_a = ['Carlos', 'Enzo']
-lista_b = lista_a.copy()
-# lista.append('Erika')
-# lista.insert(3, True)
-# lista.pop(3)
-# del lista[3]
-# lista.clear()
-# lista2 = ['Marcella']
-# lista.extend(lista2)
+# lista_a = ['Carlos', 'Enzo']
+# lista_b = lista_a.copy()
+# # lista.append('Erika')
+# # lista.insert(3, True)
+# # lista.pop(3)
+# # del lista[3]
+# # lista.clear()
+# # lista2 = ['Marcella']
+# # lista.extend(lista2)
 
-lista_a[1] = 'Lista A, alterada'
-lista_b.append('Lista A copiada')
-print(lista_a)
-print(lista_b)
+# lista_a[1] = 'Lista A, alterada'
+# lista_b.append('Lista A copiada')
+# print(lista_a)
+# print(lista_b)
+
+salas = [
+    ['Maria ', 'Elena'],
+    ['Elaine'],
+    ['Carlos', 'Enzo', 'Erika']
+]
+
+number = 1
+
+for nomes in salas:
+    print("*********************")
+    print(f'Sala {number}')
+    number += 1
+    
+    for i, sala in enumerate(nomes):
+        print(i + 1, sala)
+        
