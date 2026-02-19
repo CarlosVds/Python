7e5785607fbc3b92f6276cf2bbe3dd90f59690c3
CarlosVds <carloseduardovds@gmail.com>
2026-02-19 14:13:50 -0300

    A function executing another function

--- 06_FUNÇÃO/args.py
+++ 06_FUNÇÃO/args.py
@@ -1,17 +1,30 @@
-def soma(*args):
-    total = 1
+# def soma(*args):
+#     total = 1
     
-    for i in args:
-        total *= i
-    return total
+#     for i in args:
+#         total *= i
+#     return total
 
-print(soma(2,3))
+# print(soma(2,3))
 
-def parImpar(x):
+# def parImpar(x):
     
-    if x % 2 == 0:
-        return f'Número {x} é PAR'
-    else:
-        return f'Número {x} é IMPAR'
+#     if x % 2 == 0:
+#         return f'Número {x} é PAR'
+#     else:
+#         return f'Número {x} é IMPAR'
 
-print(parImpar(832))    
+# print(parImpar(832))
+
+def multiplier(multiplier):
+    def multiply(number):
+        return number * multiplier
+    return multiply 
+
+duplicate = multiplier(2)   
+triple = multiplier(3)   
+quadruple = multiplier(4)
+
+print(duplicate(2))   
+print(triple(2))   
+print(quadruple(2))   
