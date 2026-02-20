8966db386ac28959bd4769b8a0cc319f707dec98
CarlosVds <carloseduardovds@gmail.com>
2026-02-20 14:35:17 -0300

    Dicionary

---
+++ 07_DICT/scripts.py
@@ -0,0 +1,18 @@
+# person = {
+#     'name':'Carlos',
+#     'surname':'Santos',
+#     'age': 99,
+#     'address': [
+#         {'road':'Av Roland Garros', 'number': 123},
+#         {'road':'Road Prof Maria Hosny Curi', 'number': 44}
+#     ]  
+# }
+
+# print(person)
+
+person = {}
+
+person['name'] = "Carlos"
+person['namesurname'] = "Santos"
+
+print(person['name'], person['namesurname'])
