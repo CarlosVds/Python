3243e0cacf9e20e12a6b64933822fe60fb89d375
CarlosVds <carloseduardovds@gmail.com>
2026-02-27 09:34:02 -0300

    Set operators |,&,- and ^

--- 08_SET/scripts.py
+++ 08_SET/scripts.py
@@ -6,7 +6,18 @@ s1.add(1)
 s1.update(('Ola mundo', 1,2,3))
 s1.discard('Ola mundo')
 
-print(s1)
+# print(s1)
 
 #Useful methods
+
+s1 = {1,2,3}
+s2 = {2,3,4}
+
+s3 = s1 | s2 #Elimina osduplicado e pega o que tem de diferente em cada variavel
+s3 = s1 & s2 #Pega itens iguais em cada variavel
+s3 = s1 - s2
+s3 = s2 - s1 #Pega item unico na variavel
+s3 = s2 ^ s1 #Pega item unico de cada variavel
+
+print(s3)
 
