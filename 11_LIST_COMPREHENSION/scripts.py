905c64be03f552738236b4fdc7cc8b314ba8eed3
CarlosVds <carloseduardovds@gmail.com>
2026-03-09 14:42:42 -0300

    mapping with list comprehension

--- 11_LIST_COMPREHENSION/scripts.py
+++ 11_LIST_COMPREHENSION/scripts.py
@@ -7,22 +7,35 @@
 
 # print(*list, sep='\n')
 
-person = {
-    'name':'Carlos',
-    'surname':'Santos'
-}
+# person = {
+#     'name':'Carlos',
+#     'surname':'Santos'
+# }
 
-data_person = {
-    'age':99,
-    'adress':'Rua Teresa'
-}
+# data_person = {
+#     'age':99,
+#     'adress':'Rua Teresa'
+# }
 
-# data_complete = {**person, **data_person}
+# # data_complete = {**person, **data_person}
 
-# print(data_complete)
-def show_data(*args, **kwargs):
-    for key, value in kwargs.items():
-        print(f'{key} {value}')
+# # print(data_complete)
+# def show_data(*args, **kwargs):
+#     for key, value in kwargs.items():
+#         print(f'{key} {value}')
 
-show_data(**person, **data_person)
-  
+# show_data(**person, **data_person)
+
+produtos = [
+    {'nome': 'p1', 'preco': 20, },
+    {'nome': 'p2', 'preco': 10, },
+    {'nome': 'p3', 'preco': 30, },
+]
+
+new_product = [
+    {**product, 'preco': product['preco'] * 1.05}
+    if product['preco'] > 20 else {**product} 
+    for product in produtos
+]
+
+print(*new_product, sep='\n')  
